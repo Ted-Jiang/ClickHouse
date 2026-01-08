15602f461d48129339b331b052f595e0da911111
yangjiang <yangjiang@ebay.com>
2026-01-08 15:54:33 +0800

    Make input_format_parquet_use_native_reader_v3 and allow_iceberg_distributer_v2 default on.

--- src/Core/Settings.cpp
+++ src/Core/Settings.cpp
@@ -7001,7 +7001,7 @@ Default number of tasks for parallel reading in distributed query. Tasks are spr
     DECLARE(Bool, distributed_plan_optimize_exchanges, true, R"(
 Removes unnecessary exchanges in distributed query plan. Disable it for debugging.
 )", 0) \
-    DECLARE(Bool, allow_iceberg_distributer_v2, false, R"(
+    DECLARE(Bool, allow_iceberg_distributer_v2, true, R"(
 For test now.
 )", 0) \
     DECLARE(String, distributed_plan_force_exchange_kind, "", R"(
